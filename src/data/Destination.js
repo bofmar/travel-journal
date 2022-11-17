774b605bd3948ed025602a756a814ae89aba4ed1
bofmar <bofmar@outlook.com>
2022-11-17 12:19:24 +0200

    Add the data to be displayed

---
+++ src/data/Destination.js
@@ -0,0 +1,11 @@
+export default class Destination {
+  constructor(imageName, country, link, name, dateIn, dateOut, review) {
+    this.imageName = imageName;
+    this.country = country;
+    this.link = link;
+    this.name = name;
+    this.dateIn = dateIn;
+    this.dateOut = dateOut;
+    this.review = review;
+  }
+}
---
+++ src/data/data.js
@@ -0,0 +1,33 @@
+import Destination from './Destination';
+
+const data = [
+  new Destination(
+    'japan',
+    'Japan',
+    'https://www.google.com/maps/d/embed?mid=1W9AtcwnYAqP1yVHr5YjhwmQa0eM&ie=UTF8&msa=0&ll=35.36525600000003%2C138.737411&spn=0.09799%2C0.145912&z=12',
+    'Mount Fuji',
+    '12 Jan, 2021',
+    '24 Jan, 2021',
+    "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+  ),
+  new Destination(
+    'australia',
+    'Australia',
+    'https://www.google.com/maps/d/viewer?mid=1bvEwDA90gX7zMy2AQy5XCMfu-W0',
+    'Sydney Opera House',
+    '27 May, 2021',
+    '8 Jun, 2021',
+    "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
+  ),
+  new Destination(
+    'norway',
+    'Norway',
+    'https://www.google.com/maps/d/viewer?mid=1F48TXRDPvXIF9bPyo7cjvrYNGfw',
+    'Geirangerfjord',
+    'O1 Oct, 2021',
+    '18 Nov, 2021',
+    "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+  )
+];
+
+export default data;
